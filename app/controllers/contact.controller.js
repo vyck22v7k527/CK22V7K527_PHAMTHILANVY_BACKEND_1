893e8384407d93043bcb9014c5a89a844b14a7d6
VYPHAM <VyCK22V7K527@vlvh.ctu.edu.vn>
2024-03-09 09:52:19 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,25 @@
+exports.create = (req, res) => {
+    res.send({message: "create handler"});
+};
+ exports.findALL = (req, res) =>{
+    res.send({message: "findALL handler"});
+ };
+
+ exports.findOne = (req, res) => {
+    res.send({message: "findOne handler"});
+ };
+
+ exports.update = (req, res)=> {
+    res.send({ message: "update handler"});
+ };
+
+ exports.delete = (req, res) => {
+    res.send({message: "delete handler"});
+ };
+
+ exports.deleteAll = (req, res) => {
+    res.send({message: "deleteAll handler"});
+ };
+  exports.findALLFavorite = (req, res) => {
+    res.send({ message: "findAllFavorite handler"});
+  };
